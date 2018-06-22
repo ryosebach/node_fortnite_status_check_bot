4501d85f6185a74b17ba38f3371bb76686fe8051
ryosebach <ryo.freezia@gmail.com>
2018-06-22 11:14:53 +0900

    feat: rewrite fortnite.js in typescript

---
+++ src/fortnite.ts
@@ -0,0 +1,44 @@
+import cheerio_client from "cheerio-httpcli";
+import cron from "cron";
+import {Client as DiscordClient, Message, TextChannel} from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const token = process.env.DISCORD_TOKEN;
+const replyChannels = new Map<string, TextChannel>();
+
+const client = new DiscordClient();
+
+client.login(token);
+
+client.on("message", (mes) => {
+    if (mes.content === "!fsb start") {
+        CheckServiceStatus.start();
+        (mes.channel as TextChannel).send("メンテ状況の監視を始めるよ");
+        replyChannels.set((mes.channel as TextChannel).id, mes.channel as TextChannel);
+    }
+    if (mes.content === "!fsb stop") {
+        CheckServiceStatus.stop();
+        replyChannels.delete((mes.channel as TextChannel).id);
+        (mes.channel as TextChannel).send("メンテ状況の監視を終わるよ");
+    }
+});
+
+const CheckServiceStatus = new cron.CronJob("*/3 * * * * *", async () => {
+    const $ = (await cheerio_client.fetch("https://status.epicgames.com")).$;
+    const underMainte = await $("span.component-status").filter(function(this: void) {
+        return !$(this).text().match(/Operational/);
+    });
+    if (underMainte.length !== 0) { return; }
+    replyChannels.forEach((channel: TextChannel, id: string) => {
+        channel.send("メンテ終わり！Fortnite遊べるよ！");
+    });
+    replyChannels.clear();
+
+    CheckServiceStatus.stop();
+},
+    undefined,
+    false,
+    "Asia/Tokyo",
+);
